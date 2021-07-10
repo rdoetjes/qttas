2acf9b0797283fa69e3bd867ab7e2110f43f167e
Raymond Doetjes <rdoetjes@phonax.com>
2021-07-10 14:29:17 +0200

    added the cleaner code using, matrix decision, after filming the video

--- gamelogic.cpp
+++ gamelogic.cpp
@@ -1,7 +1,36 @@
 #include "gamelogic.h"
-#include <iostream>
+
+using namespace std;
+typedef vector< vector<int> > Matrix;
+
+
+void processImage(QImage *img, Matrix &decision, int *gameObjectsToTrack){
+   QColor pix;
+   for (long unsigned int i=0; i < decision.size(); i++){
+       pix = img->pixelColor( decision[i][0], decision[i][1]);
+       if (pix.red() < decision[i][2]) gameObjectsToTrack[ decision[i][3] ] = decision[i][4];
+   }  
+
+}
 
 void getMyPosition(QImage *img, int *myPosition){
+    Matrix decision = {
+        {858, 798, 40, 0, 1},
+        {984, 798, 40, 1, 1},
+        {1106, 798, 40, 2, 1},
+        {1233, 798, 40, 4, 1},
+        {733, 798, 40, 3, 1}
+
+    };
+
+    processImage(img, decision, myPosition);
+
+ /*
+  * This was the code as it was shown in the video
+    As to easily describe the logic
+    The above code does the smae but is cleaner
+    (not faster though!)
+
    QColor pix = img->pixelColor(858, 798);
    if (pix.red() <40) myPosition[0] = 1;                    //demo for video
 
@@ -16,9 +45,24 @@ void getMyPosition(QImage *img, int *myPosition){
 
    pix = img->pixelColor(733, 798);                         //hitchiker position car
    if (pix.red() <40) myPosition[3] = 1; 
+   */
 }
 
 void getObstacles(QImage *img, int *obstacles){
+   Matrix decision = { 
+       {852, 735, 40, 0, 1},
+       {960, 712, 40, 1, 1},
+       {1030, 726, 40, 2, 1} 
+   }; 
+    
+    processImage(img, decision, obstacles);
+
+  /*
+  * This was the code as it was shown in the video
+    As to easily describe the logic
+    The above code does the smae but is cleaner
+    (not faster though!)
+
    QColor pix = img->pixelColor(852, 735);
    if (pix.red() <40) obstacles[0] = 1; 
 
@@ -27,13 +71,14 @@ void getObstacles(QImage *img, int *obstacles){
 
    pix = img->pixelColor(1030,726);
    if (pix.red() <40) obstacles[2] = 1; 
+   */
 }
 
 int movesToSafePlace(int pos, int *obstacles){
     //when beginning we need to be very cary to merge into traffic
     if (pos == 4) if (obstacles[2] == 0) return -1; 
 
-    //We have a bias for the fasdt lane :P
+    //We have a bias for the fast lane :P
     if (obstacles[0] == 0) return 0 - pos; 
 
     if (obstacles[1] == 0) return 1 - pos;
--- gamelogic.h
+++ gamelogic.h
@@ -3,6 +3,7 @@
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
 #include <X11/extensions/XTest.h>
+#include <vector>
 
 
 void gameLogic(QPixmap *screen);
